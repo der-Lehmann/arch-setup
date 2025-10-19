090d557cce8ed95a3f3678a1ee4d062d6c8dc336
Lars Lehmann <larsh.lehmann64@gmail.com>
2025-10-19 11:00:14 +0200

    Yay wird installiert.

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ set -e
 
 DIRECTORY="$HOME/.local/share/arch-setup"
 
+source $DIRECTORY/install/yay.sh
 source $DIRECTORY/install/packages.sh
 source $DIRECTORY/install/walker.sh
 source $DIRECTORY/install/display-manager.sh
