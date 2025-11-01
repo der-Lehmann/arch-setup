100d9ff4bdef507b22b7eccce0fad4ef420f07bf
Lars Lehmann <larsh.lehmann64@gmail.com>
2025-11-01 19:38:40 +0100

    Docker wird installiert.

--- install/devtools.sh
+++ install/devtools.sh
@@ -1,3 +1,10 @@
+sudo pacman -Sy --noconfirm --needed \
+    docker \
+    docker-buildx \
+    docker-compose
+
+sudo systemctl enable docker
+
 # tmux
 mkdir -p ~/.config/tmux/plugins/catppuccin
 git clone -b v2.1.3 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
