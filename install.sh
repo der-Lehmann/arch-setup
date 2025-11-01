b97aa4258f3e200ce97e1fdb68ee7336f8ca0a96
Lars Lehmann <larsh.lehmann64@gmail.com>
2025-11-01 18:18:06 +0100

    Benötigte Schriftarten werden installiert.

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ sudo pacman -Syu
 source $DIRECTORY/install/yay.sh
 source $DIRECTORY/install/packages.sh
 source $DIRECTORY/install/display-manager.sh
+source $DIRECTORY/install/fonts.sh
 source $DIRECTORY/install/devtools.sh
 
 source $DIRECTORY/install/config.sh
---
+++ install/fonts.sh
@@ -0,0 +1,14 @@
+cd /tmp
+sudo rm -rf /tmp/font
+mkdir /tmp/font
+cd /tmp/font
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/JetBrainsMono.zip
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Meslo.zip
+unzip -o /tmp/JetBrainsMono.zip -d /tmp/font
+unzip -o /tmp/Meslo.zip -d /tmp/font
+
+sudo cp -f /tmp/font/*.ttf /usr/local/share/fonts/
+sudo fc-cache -f -v
+sudo rm -rf /tmp/font
+cd -
