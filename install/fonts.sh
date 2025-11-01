3ffa0b3ee0f8cc4aa941513675f181a37fd2d143
Lars Lehmann <larsh.lehmann64@gmail.com>
2025-11-01 21:45:20 +0100

    Fehler im Skript zur Installation der Schriftarten behoben.

--- install/fonts.sh
+++ install/fonts.sh
@@ -5,10 +5,11 @@ cd /tmp/font
 
 curl -L -O https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/JetBrainsMono.zip
 curl -L -O https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Meslo.zip
-unzip -o /tmp/JetBrainsMono.zip -d /tmp/font
-unzip -o /tmp/Meslo.zip -d /tmp/font
+unzip -o ./JetBrainsMono.zip -d /tmp/font
+unzip -o ./Meslo.zip -d /tmp/font
 
-sudo cp -f /tmp/font/*.ttf /usr/local/share/fonts/
+sudo mkdir -p /usr/share/fonts/TTF
+sudo cp -f /tmp/font/*.ttf /usr/share/fonts/TTF/
 sudo fc-cache -f -v
 sudo rm -rf /tmp/font
 cd -
