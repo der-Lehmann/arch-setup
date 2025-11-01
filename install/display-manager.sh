a882c4c828a6a9143307fd511684f7eb7d4a6ea9
Lars Lehmann <larsh.lehmann64@gmail.com>
2025-11-01 13:09:30 +0100

    Autologin eingerichtet.

--- install/display-manager.sh
+++ install/display-manager.sh
@@ -2,4 +2,14 @@
 
 # Set up SDDM display manager for graphical login
 echo "Setting up SDDM display manager..."
+sudo mkdir -p /etc/sddm.conf.d
+
+if [ ! -f /etc/sddm.conf.d/autologin.conf ]; then
+  cat <<EOF | sudo tee /etc/sddm.conf.d/autologin.conf
+[Autologin]
+User=$USER
+Session=hyprland-uwsm
+EOF
+fi
+
 sudo systemctl enable sddm.service
