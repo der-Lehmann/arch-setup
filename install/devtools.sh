268b33ee53789e8f8bfacac7f9f352bea3867885
Lars Lehmann <larsh.lehmann64@gmail.com>
2025-11-01 19:38:16 +0100

    nodejs wird installiert.

--- install/devtools.sh
+++ install/devtools.sh
@@ -1,3 +1,9 @@
+# tmux
 mkdir -p ~/.config/tmux/plugins/catppuccin
-
 git clone -b v2.1.3 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
+
+# node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
+nvm install node
