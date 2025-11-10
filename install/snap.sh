8809783c0de3f9be8bd26c61b546bd6501cf5e82
Lars Lehmann <larsh.lehmann64@gmail.com>
2025-11-10 20:39:36 +0100

    Snap installation angepasst.

--- install/snap.sh
+++ install/snap.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
+TMPDIR=$(mktemp -d)
+cd "$TMPDIR"
 
 git clone https://aur.archlinux.org/snapd.git
 cd snapd
+
 makepkg -s --noconfirm
 sudo pacman -U --noconfirm *.pkg.tar.zst
 
@@ -10,4 +13,7 @@ sudo systemctl enable --now snapd.socket
 
 sudo systemctl enable --now snapd.apparmor.service
 
-sudo ln -s /var/lib/snapd/snap /snap
+sudo ln -sf /var/lib/snapd/snap /snap
+
+cd /
+rm -rf "$TMPDIR"
