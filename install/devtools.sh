e43479317ca61f4f6859ac84ad38a3c76c2a10ff
Lars Lehmann <larsh.lehmann64@gmail.com>
2026-08-02 19:43:04 +0200

    Removed second docker install.

--- install/devtools.sh
+++ install/devtools.sh
@@ -4,19 +4,6 @@ CHSH=no RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-# docker
-sudo pacman -Sy --noconfirm --needed docker docker-buildx docker-compose 
-sudo systemctl enable docker
-sudo mkdir -p /etc/docker
-sudo tee /etc/docker/daemon.json >/dev/null <<'EOF'
-{
-    "log-driver": "json-file",
-    "log-opts": { "max-size": "10m", "max-file": "5" },
-    "dns": ["172.17.0.1"],
-    "bip": "172.17.0.1/16"
-}
-EOF
-
 # tmux
 mkdir -p ~/.config/tmux/plugins/catppuccin
 git clone -b v2.1.3 https://github.com/catppuccin/tmux.git ~/.config/tmux/plugins/catppuccin/tmux
