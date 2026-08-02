f4052697467ec54fc5b9cbdd96df44df7b9f95d9
Lars Lehmann <larsh.lehmann64@gmail.com>
2026-08-02 19:44:15 +0200

    Removed disabling the systemd-networkd since this lead to errors.

--- install/networkmanager.sh
+++ install/networkmanager.sh
@@ -5,11 +5,4 @@ sudo tee /etc/NetworkManager/conf.d/wifi_backend.conf >/dev/null <<'EOF'
 wifi.backend=iwd
 EOF
 
-# NetworkManager owns networking; disable systemd-networkd so wait-online
-# doesn't block graphical.target (and delay UWSM/Hyprland startup).
-sudo systemctl disable --now \
-  systemd-networkd.service \
-  systemd-networkd.socket \
-  systemd-networkd-wait-online.service
-
 sudo systemctl enable NetworkManager.service
